7f6115bc431005657f2308098ec9d0da49f50013
Mihnea <mihneaaleman@yahoo.com>
2020-10-17 19:41:32 +0300

    finished ex4

---
+++ Seminar3/ex4.py
@@ -0,0 +1,30 @@
+def find_word(matrix,word,i,j,num,beforeI,beforeJ):
+    if num==len(word):
+        print (True)
+    else:
+        if matrix[i-1][j]==word[num] and i-1!=beforeI:
+            find_word(matrix,word,i-1,j,num+1,i,j)
+        if j+1<len(matrix[i])  and matrix[i][j+1] == word[num] and j+1!=beforeJ :
+            find_word(matrix, word, i, j+1, num + 1, i,j)
+        if  i+1<len(matrix)  and matrix[i+1][j] == word[num] and i+1!=beforeI:
+            find_word(matrix, word, i +1, j, num + 1,i,j)
+        if matrix[i][j-1] == word[num] and j-1!=beforeJ:
+            find_word(matrix, word, i, j-1, num + 1,i,j)
+
+def main():
+    word = "ALARM"
+    matrix = [
+        ['A', 'B', 'C', 'D'],
+        ['L', 'A', 'G', 'H'],
+        ['C', 'R', 'M', 'Z']
+    ]
+    for a in range(0,len(matrix)):
+        for b in range(0,len(matrix[a])):
+            if matrix[a][b]==word[0]:
+                find_word(matrix,word,a,b,1,0,0)
+
+
+
+main()
+
+
